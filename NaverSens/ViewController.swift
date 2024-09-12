08cf86cb92928c3c198e5898b257ed29f8b19452
jahyeang <qkrwpgud1121@gmail.com>
2024-09-12 17:48:31 +0900

    Initial Commit

---
+++ NaverSens/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NaverSens
+//
+//  Created by 박제형 on 9/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
